d147c50d1cd02a2504ec1fb90da21673bdfdceed
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-05-30 18:39:00 -0300

    feat: Creating the initials models of Character and the Enum type of characters

---
+++ WebApi/Models/RpgClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApi.Models
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight  = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
