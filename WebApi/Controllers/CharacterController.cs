2a276597d6e37c8e8b06f3e7cd8277a1a9bf1a64
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-05-30 18:41:19 -0300

    feat: Creating the initial CharacterController with two get routes and one post route

---
+++ WebApi/Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Id = 1, Name= "San"} 
+        };
+
+        [HttpGet("GetAll")]
+        public ActionResult<List<Character>> Get()
+        { 
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter) 
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
