49d3227ab62632af085d9c2b59f956567d6a1bd8
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-05-31 16:16:59 -0300

    feat: creating the Services folde, the CharacterService Interface and class.

---
+++ WebApi/Services/CharacterService/CharacterService.cs
@@ -0,0 +1,29 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services.CharacterService
+{
+    public class CharacterService : ICharacterService
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Id = 1, Name= "San"}
+        };
+
+        public List<Character> AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return characters;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return characters;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            return characters.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
---
+++ WebApi/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,13 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+
+        Character GetCharacterById(int id);
+
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
