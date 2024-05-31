424ec6cb9a3e26caf72f7c3724296f05e784088c
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-05-31 18:51:10 -0300

    feat: creating the DTOs of GetCharacter and AddCharacter

---
+++ WebApi/Dtos/Character/AddCharacterDto.cs
@@ -0,0 +1,14 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Dtos.Character
+{
+    public class AddCharacterDto
+    {
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 100;
+        public int Strenght { get; set; } = 10;
+        public int Defense { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public RpgClass Class { get; set; } = RpgClass.Knight;
+    }
+}
