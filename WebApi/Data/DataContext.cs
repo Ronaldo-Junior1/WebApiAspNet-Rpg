fb8c8b631ec630eeddc5e589832c1ee2715cc6ea
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2024-06-01 21:50:50 -0300

    Creating the connection and the first migration with the database

---
+++ WebApi/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+        
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
 using WebApi.Services.CharacterService;
 
 namespace WebApi
@@ -10,7 +12,8 @@ namespace WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
+            builder.Services.AddDbContext<DataContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
